72afe2996e3adbb5bacb346d0b6e7a00350f59f6
Fernando <asialer05@hotmail.com>
2025-01-03 13:32:23 -0500

    Proyecto base

---
+++ src/main/java/com/fernando/ms/shipments/app/dfood_shipments_service/DfoodShipmentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.shipments.app.dfood_shipments_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfoodShipmentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfoodShipmentsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/ms/shipments/app/dfood_shipments_service/DfoodShipmentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.shipments.app.dfood_shipments_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfoodShipmentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
